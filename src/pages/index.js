37ccd9e9837ea8f978814879ae30a57d16eac05d
pungggi <alessandro.pungitore@gmail.com>
2019-02-24 22:37:52 +0100

    reduce to minimum

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,8 @@
 import React from "react";
 import { view } from "react-easy-state";
-import Layout from "../components/layout";
 
 export default view(() => (
-  <Layout>
+  <div>
     <h1>Hello</h1>
-    <p>Welcome to your new site.</p>
-  </Layout>
+  </div>
 ));
